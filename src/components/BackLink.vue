3ed3be076ff04d4e5f672856539b58a057b1e38b
Sergey <boobassbooster@gmail.com>
2024-04-27 21:54:28 +0300

    fix back link

--- src/components/BackLink.vue
+++ src/components/BackLink.vue
@@ -1,25 +1,45 @@
 <script setup lang="ts">
 import IconChevronLeft from '@/components/icons/IconChevronLeft.vue'
+import { useRouter } from 'vue-router'
 
 interface Props {
   title?: string;
+  titleNoHistory?: string;
   to?: string;
 }
 
 withDefaults(defineProps<Props>(), {
   title: 'Назад',
+  titleNoHistory: 'На главную',
   to: '/'
 })
+
+const noHistory = !window.history.state.back
+
+const router = useRouter()
+const onClick = (navigate: Function) => {
+  noHistory
+    ? navigate()
+    : router.back()
+}
 </script>
 
 <template>
   <div class="back-link">
     <IconChevronLeft class="back-link__icon" />
     <RouterLink
+      v-slot="{ href, navigate }"
       class="back-link__to"
       :to="to"
+      custom
     >
-      {{ title }}
+      <a
+        v-bind="$attrs"
+        :href="href"
+        @click.prevent="onClick(navigate)"
+      >
+        {{ noHistory ? titleNoHistory : title }}
+      </a>
     </RouterLink>
   </div>
 </template>
